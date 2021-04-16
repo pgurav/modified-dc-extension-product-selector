831588427d01ef653ea58567aee4945aa29b587a
Priyanka Gurav <guravpriyankap@gmail.com>
2021-04-16 14:15:32 +0530

    added condition to check item lenth, if less than 20 then no pagination buttons should show

--- src/pager/Pager.js
+++ src/pager/Pager.js
@@ -32,30 +32,40 @@ export const PagerComponent = ({ changePage, page: { numPages, curPage } }) => {
   }
   
 
-  return (
-    <ButtonGroup color="primary" className={classes.root}>
-
-    {pages.map(page => (
-        <Button variant={page === curPage ? 'contained' : null} key={page} onClick={() => changePage(page)}>
-          {page + 1}
+  if (items.length > 20) {
+    return (
+      <ButtonGroup color="primary" className={classes.root}>
+        <Button aria-label="first" onClick={() => changePage(0)} disabled={curPage === 0}>
+          <SkipPrevious fontSize="small" />
         </Button>
-      ))}
-      
-      <Button aria-label="first" onClick={() => changePage(0)} disabled={curPage === 0}>
-        <SkipPrevious fontSize="small" />
-      </Button>
-      <Button aria-label="previous" onClick={() => changePage(curPage - 1)} disabled={curPage === 0}>
-        <ArrowLeft fontSize="small" />
-      </Button>
-      
-      <Button aria-label="next" onClick={() => changePage(curPage + 1)} disabled={curPage === numPages - 1}>
-        <ArrowRight fontSize="small" />
-      </Button>
-      <Button aria-label="last" onClick={() => changePage(numPages - 1)} disabled={curPage === numPages - 1}>
-        <SkipNext fontSize="small" />
-      </Button>
-    </ButtonGroup>
-  );
+        <Button aria-label="previous" onClick={() => changePage(curPage - 1)} disabled={curPage === 0}>
+          <ArrowLeft fontSize="small" />
+        </Button>
+        {pages.map(page => (
+          <Button variant={page === curPage ? 'contained' : null} key={page} onClick={() => changePage(page)}>
+            {page + 1}
+          </Button>
+        ))}
+        <Button aria-label="next" onClick={() => changePage(curPage + 1)} disabled={curPage === numPages - 1}>
+          <ArrowRight fontSize="small" />
+        </Button>
+        <Button aria-label="last" onClick={() => changePage(numPages - 1)} disabled={curPage === numPages - 1}>
+          <SkipNext fontSize="small" />
+        </Button>
+      </ButtonGroup>
+    );
+  } else {
+    return (
+      <ButtonGroup color="primary" className={classes.root}>
+        {pages.map(page => (
+          <Button variant={page === curPage ? 'contained' : null} key={page} onClick={() => changePage(page)}>
+            {page + 1}
+          </Button>
+        ))}
+      </ButtonGroup>
+    );
+  }
+  
   
 };
 
